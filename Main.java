15c747ef9b9d6ed51c6a4f1b11bd90789ecefa16
mirceanagy <mirceanagy@yahoo.co.uk>
2020-09-02 21:53:26 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,48 +8,56 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    private static final int PLACE_HIERARCHIES = 50;
-    private static final int PLACE_HIERARCHY_DEPTH = 100;
-    private static final int CONTENT_PER_PLACE = 30;
-    private static final int GROUPS = 100;
-    private static final int USERS_PER_GROUP = 100;
+    private static final int MULTIPLIER = 10;
 
     public static void main(String[] args) {
         List<List<String>> vertices = new ArrayList<>();
         List<List<String>> edges = new ArrayList<>();
+        List<String> places = new ArrayList<>();
+        List<String> users = new ArrayList<>();
+        List<List<String>> placeAndUserPairs = new ArrayList<>();
         vertices.add(Vertex.toCsvLineHeader());
         edges.add(Edge.toCsvLineHeader());
-        for (int hierarchy = 0; hierarchy < PLACE_HIERARCHIES; hierarchy++) {
-            for (int hierarchyDepth = 0; hierarchyDepth < PLACE_HIERARCHY_DEPTH; hierarchyDepth++) {
+        for (int hierarchy = 0; hierarchy < MULTIPLIER; hierarchy++) {
+            for (int hierarchyDepth = 0; hierarchyDepth < MULTIPLIER; hierarchyDepth++) {
                 String placeId = "place_" + hierarchy + "_" + hierarchyDepth;
                 String placeParentId = "place_" + hierarchy + "_" + (hierarchyDepth - 1);
                 vertices.add(new Vertex(placeId, "place", placeId, placeId).toCsvLine());
                 if (hierarchyDepth != 0) {
                     edges.add(new Edge("parent_" + placeId, placeId, placeParentId, "parent").toCsvLine());
                 }
-                for (int content = 0; content < CONTENT_PER_PLACE; content++) {
+                if (hierarchyDepth == MULTIPLIER - 1) {
+                    places.add(placeId);
+                }
+                for (int content = 0; content < MULTIPLIER; content++) {
                     String contentId = "content_" + hierarchy + "_" + hierarchyDepth + "_" + content;
                     vertices.add(new Vertex(contentId, "content", contentId, contentId).toCsvLine());
                     edges.add(new Edge("content_place_" + contentId, contentId, placeId, "placed").toCsvLine());
                 }
             }
         }
-        for (int group = 0; group < GROUPS; group++) {
+        for (int group = 0; group < MULTIPLIER; group++) {
             String groupId = "group_" + group;
             vertices.add(new Vertex(groupId, "group", groupId, groupId).toCsvLine());
-            edges.add(new Edge("group_place" + group, groupId, "place_" + Math.round(Math.random() * (PLACE_HIERARCHIES-1) )+ "_0", "read").toCsvLine());
+            edges.add(new Edge("group_place" + group, groupId, "place_" + group + "_0", "read").toCsvLine());
             if (Math.random() < 0.5) {
-                edges.add(new Edge("group_place" + groupId, groupId, "place_" + Math.round(Math.random() * (PLACE_HIERARCHIES-1) )+ "_0", "write").toCsvLine());
+                edges.add(new Edge("group_place" + groupId, groupId, "place_" + group + "_0", "write").toCsvLine());
             }
-            for (int user = 0; user < USERS_PER_GROUP; user++) {
+            for (int user = 0; user < MULTIPLIER; user++) {
                 String userId = "user_" + group + "_" + user;
+                users.add(userId);
                 vertices.add(new Vertex(userId, "user", userId, userId).toCsvLine());
                 edges.add(new Edge("user_group_" + userId, userId, groupId, "memberOf").toCsvLine());
             }
         }
-        exportCsv(vertices, "c:/dev/vertex.csv");
-        exportCsv(edges, "c:/dev/edges.csv");
-
+        for (String place : places) {
+            for (String user : users) {
+                placeAndUserPairs.add(Arrays.asList(place, user));
+            }
+        }
+//        exportCsv(vertices, "c:/dev/vertices" + MULTIPLIER + ".csv");
+//        exportCsv(edges, "c:/dev/edges" + MULTIPLIER + ".csv");
+        exportCsv(placeAndUserPairs, "c:/dev/variables" + MULTIPLIER + ".csv");
     }
 
     private static void exportCsv(List<List<String>> data, String fileName) {
@@ -63,7 +71,7 @@ public class Main {
         }
     }
 
-    public static String convertToCSV(List<String> data) {
+    private static String convertToCSV(List<String> data) {
         return data.stream()
 //                .map(Main::escapeSpecialCharacters)
                 .collect(Collectors.joining(","));
@@ -84,7 +92,7 @@ public class Main {
         String name;
         String description;
 
-        public Vertex(String id, String label, String name, String description) {
+        Vertex(String id, String label, String name, String description) {
             this.id = id;
             this.label = label;
             this.name = name;
@@ -106,7 +114,7 @@ public class Main {
         String to;
         String label;
 
-        public Edge(String id, String from, String to, String label) {
+        Edge(String id, String from, String to, String label) {
             this.id = id;
             this.from = from;
             this.to = to;
